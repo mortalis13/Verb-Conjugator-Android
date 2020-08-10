c2223e225abc60e68af88c56c6b2ac5896ef73a0
mortalis13 <stangerroman@gmail.com>
2020-08-10 13:27:15 +0200

    -changed icon

--- german/-b.bat
+++ german/-b.bat
@@ -1,4 +1,13 @@
 @echo off
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
 echo Gradle Android [Build]...
 
 call gradle assembleDebug
--- german/-r.bat
+++ german/-r.bat
@@ -4,5 +4,5 @@ echo Gradle Android [Run]...
 rem Gradle tasks
 rem assembleDebug, installDebug, uninstallDebug
 
-gradle appStart
 rem gradle installDebug
+gradle appStart
